bdd7acb12d38a3d07a410b042300752f02968347
Ben Saunders <54156568+benedictsaunders@users.noreply.github.com>
2021-05-05 15:22:59 +0100

    Add files via upload

---
+++ conf_search_gh.bash
@@ -0,0 +1,37 @@
+#!/bin/bash -l
+
+SMILESFILE=smiles-list.txt
+NAMESFILE=names-list.txt
+
+
+if test -f "$NAMESFILE"; then
+    readarray names < $NAMESFILE
+    nameexist=true
+else
+    echo "No $NAMEFILE file found. Naming shall be numeric."
+fi
+if test -f "$SMILESFILE"; then
+    readarray lines < $SMILESFILE
+    for i in "${lines[@]}"
+    do
+        echo $i
+        for j in "${!lines[@]}"
+        do
+            if [ "${lines[$j]}" = "${i}" ] ; then
+                if [ "$nameexist" = true ] ; then
+                    dirname="${names[$j]}"
+                else
+                    dirname=$j
+                fi
+                mkdir $dirname
+                echo $dirname
+                cd $dirname
+                python /home/uccabsa/scripts/conf_search_min.py -s $i -n 15000
+                cd ../
+            fi
+        done
+    done
+else
+    python /home/uccabsa/scripts/conf_search_min.py -s $1 -n 15000
+    cd ../
+fi
