635542df4e030a5a3d75d82ce72761ad95d5a1d0
Benedict Saunders <benedictsaunders@gmail.com>
2024-04-23 18:09:29 +0100

    Added dimension counter

--- AIMS/constrain_2D.py
+++ AIMS/constrain_2D.py
@@ -8,8 +8,10 @@ while True:
         lattice = lines[idx].split()[1:]
         if dim in [0, 1]:
             lines.insert(idx+1, "constrain_relaxation z")
+            dim += 1
         elif dim == 2:
             lines.insert(idx+1, "constrain_relaxation .true.")
+            dim += 1
         else:
             break
     idx += 1
