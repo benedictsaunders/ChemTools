3aeb443631d30b0101da088b6bf1bbfaf58c5170
Benedict Saunders <benedictsaunders@gmail.com>
2024-04-23 19:20:01 +0100

    Constrain check

--- AIMS/constrain_2D.py
+++ AIMS/constrain_2D.py
@@ -5,15 +5,19 @@ idx = 0
 dim = 0
 while True:
     if lines[idx].strip().startswith("lattice"):
-        lattice = lines[idx].split()[1:]
-        if dim in [0, 1]:
-            lines.insert(idx+1, "constrain_relaxation z")
-            dim += 1
-        elif dim == 2:
-            lines.insert(idx+1, "constrain_relaxation .true.")
-            dim += 1
+        if not lines[idx+1].strip().startswith("lattice_vector"):
+            lattice = lines[idx].split()[1:]
+            if dim in [0, 1]:
+                lines.insert(idx+1, "constrain_relaxation z")
+                dim += 1
+            elif dim == 2:
+                lines.insert(idx+1, "constrain_relaxation .true.")
+                dim += 1
+            else:
+                break
         else:
-            break
+            dim += 1
+            continue
     idx += 1
     if idx == len(lines):
         break
